6aee123c247fa8e02ab1fea20803cbb198e69dff
gvurimi <38063579+gvurimi@users.noreply.github.com>
2018-04-03 23:20:18 -0700

    Add files via upload

---
+++ questions.scm
@@ -0,0 +1,102 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+; Some utility functions that you may find useful to implement.
+(define (map proc items)
+   (if (null? items)
+    nil
+    (cons (proc (car items)) (map proc (cdr items)))
+  )
+)
+
+(define (cons-all first rests)
+  (map (lambda (x) 
+  (cons first x)) rests))
+(define (zip pairs)
+  (list (map car pairs) 
+  (map cadr pairs)))
+
+;; Problem 17
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 17
+  
+  (define i (- 1))
+  (define (count s i)
+      (cond
+        ((null? s) nil)
+        (else
+          (define i (+ i 1))
+          (cons (cons i (cons (car s) nil))(count (cdr s) i))
+        )
+      )
+    )
+    (count s i)
+  )
+  ; END PROBLEM 17
+
+;; Problem 18
+;; List all ways to make change for TOTAL with DENOMS
+(define (list-change total denoms)
+  ; BEGIN PROBLEM 18
+  (cond ((= total 0) nil)
+    ((null? denoms) nil)
+    ((= total (car denoms))
+      (cons (list (car denoms)) 
+        (list-change total (cdr denoms))))
+    ((> total (car denoms)) 
+      (append (cons-all (car denoms) 
+        (list-change (- total (car denoms)) denoms)) 
+      (list-change total (cdr denoms))))
+    (else 
+      (list-change total (cdr denoms))))
+  )
+  
+  ; END PROBLEM 18
+
+;; Problem 19
+;; Returns a function that checks if an expression is the special form FORM
+(define (check-special form)
+  (lambda (expr) (equal? form (car expr))))
+
+(define lambda? (check-special 'lambda))
+(define define? (check-special 'define))
+(define quoted? (check-special 'quote))
+(define let?    (check-special 'let))
+
+;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define (let-to-lambda expr)
+  (cond ((atom? expr)
+         ; BEGIN PROBLEM 19
+         expr
+         ; END PROBLEM 19
+         )
+        ((quoted? expr)
+         ; BEGIN PROBLEM 19
+         expr
+         ; END PROBLEM 19
+         )
+        ((or (lambda? expr)
+             (define? expr))
+         (let ((form   (car expr))
+               (params (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 19
+           (cons form (cons params (let-to-lambda body)))
+           ; END PROBLEM 19
+           ))
+        ((let? expr)
+         (let ((values (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 19
+           (cons (list 'lambda (car (zip values)) (let-to-lambda (car body)))
+                  (map let-to-lambda (cadr (zip values))))
+           ; END PROBLEM 19
+           ))
+        (else
+         ; BEGIN PROBLEM 19
+         (map let-to-lambda expr)
+         ; END PROBLEM 19
+         )))
